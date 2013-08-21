905fbe0c5e4d6bb34f493c873fd70c1535fa428b
Katsunori SUZUI <7kry.yn@gmail.com>
2013-08-22 05:35:58 +0900

    JSファイルにライセンス明記

--- nagato_emergency.js
+++ nagato_emergency.js
@@ -1,3 +1,22 @@
+// (C) 2013 7kry (Katsunori SUZUI) <http://7kry.net>
+//
+// GitHub repository: https://github.com/7kry/nagato_emergency
+//
+//                              -*- LICENSE -*-
+//
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+//
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+//
+//     The complete text of the GNU General Public License version 3
+//     can be found in http://www.gnu.org/licenses/gpl-3.0.en.html
+
 function nagato_emergency(id, duration, ok, cancel, message){
 	var htmlesc = function(c){
 		switch(c){
